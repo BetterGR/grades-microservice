968fb0a89f799f3f6d0c4c048e21707c0e7738b0
zaheramir <102883383+zaheramir@users.noreply.github.com>
2025-06-04 11:48:09 +0300

    Update server.go

--- server/server.go
+++ server/server.go
@@ -245,10 +245,9 @@ func main() {
 	klog.InitFlags(nil)
 	flag.Parse()
 
-	err := godotenv.Load()
-	if err != nil {
-		klog.Fatalf("Error loading .env file")
-	}
+	if err := godotenv.Load(); err != nil {
+    klog.Warning("Warning: No .env file loaded, proceeding with environment variables only")
+}
 
 	// Initialize the server.
 	server, err := initGradesMicroserviceServer()
